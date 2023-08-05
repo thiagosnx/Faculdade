88cd784459a0e4c0b4d4a73e7472fb2c1bba5780
Rapha29 <122626805+Rapha29@users.noreply.github.com>
2023-08-04 23:03:46 -0300

    cw1

---
+++ 22 - Linguagem de Programacao/08 - CodigosDaMateria/CW-0110 Função definidas pelo usuário.py
@@ -0,0 +1,24 @@
+# Funções, também conhecidas como subprogramas ou sub-rotinas, são pequenos blocos de 
+# código aos quais se dá um nome, desenvolvidos para resolver tarefas específicas.
+'''A sintaxe de uma função em Python é feita com:
+A palavra reservada def.
+O nome da função.
+Os parênteses que indicam se existem ou não parâmetros para a função.
+E o comando return (que é opcional);'''
+
+# Existem três sintaxes diferentes:
+'''
+# 1 - Uma função que não recebe e não retorna valores.
+def nome_funcao():
+    #bloco de comandos
+
+# 2 - Uma função que não recebe argumento, mas retorna valores.
+def nome_funcao():
+    #bloco de comandos
+    Return valor 
+    
+# 3 - Uma função que recebe argumento e retorna valor.
+def nome_funcao(param1, param2)
+    #bloco de comandos
+    Return valor     
+'''
