835c4167ab106c90434a09f3a53a6366dcb9918f
Rapha29 <122626805+Rapha29@users.noreply.github.com>
2023-08-06 11:15:58 -0300

    cmt

--- 22 - Linguagem de Programacao/08 - CodigosDaMateria/CW-0112 Switch Case - match.py
+++ 22 - Linguagem de Programacao/08 - CodigosDaMateria/CW-0112 Switch Case - match.py
@@ -1,6 +1,6 @@
-# Similar ao Switch case o Python possui o match case
+# Similar ao Switch case o Python possui o match case, porem ele só retorna similaridades
 
-estacao = "primavera"
+estacao = "outono"
 
 match estacao:
     
@@ -10,6 +10,8 @@ match estacao:
         print("O Verao é quente")
     case "inverno":
         print("Estação das Flores")
-    case "verao":
+    case "outono":
         print("O outono é sempre igual, as floques caem no quintal")
+    case _:
+        print("Digite uma estação válida")
         
