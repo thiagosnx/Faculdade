e0f479c0362a34da55870cceb5a8d83a4efbb295
Rapha29 <122626805+Rapha29@users.noreply.github.com>
2023-08-06 11:05:49 -0300

    matchCase

---
+++ 22 - Linguagem de Programacao/08 - CodigosDaMateria/CW-0112 Switch Case - match.py
@@ -0,0 +1,15 @@
+# Similar ao Switch case o Python possui o match case
+
+estacao = "primavera"
+
+match estacao:
+    
+    case "primavera":
+        print("Estação das Flores")
+    case "verao":
+        print("O Verao é quente")
+    case "inverno":
+        print("Estação das Flores")
+    case "verao":
+        print("O outono é sempre igual, as floques caem no quintal")
+        
