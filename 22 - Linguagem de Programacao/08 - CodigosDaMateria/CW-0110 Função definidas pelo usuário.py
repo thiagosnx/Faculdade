a1aa018641307767e7527f71ab895e30780a6ab2
Rapha29 <122626805+Rapha29@users.noreply.github.com>
2023-08-06 10:30:02 -0300

    chngs

--- 22 - Linguagem de Programacao/08 - CodigosDaMateria/CW-0110 Função definidas pelo usuário.py
+++ 22 - Linguagem de Programacao/08 - CodigosDaMateria/CW-0110 Função definidas pelo usuário.py
@@ -7,18 +7,17 @@ Os parênteses que indicam se existem ou não parâmetros para a função.
 E o comando return (que é opcional);'''
 
 # Existem três sintaxes diferentes:
-'''
+
 # 1 - Uma função que não recebe e não retorna valores.
-def nome_funcao():
+#def nome_funcao1():
     #bloco de comandos
 
 # 2 - Uma função que não recebe argumento, mas retorna valores.
-def nome_funcao():
+#def nome_funcao2():
     #bloco de comandos
-    Return valor 
+#    return valor 
     
 # 3 - Uma função que recebe argumento e retorna valor.
-def nome_funcao(param1, param2)
+#def nome_funcao3(param1, param2)
     #bloco de comandos
-    Return valor     
-'''
+#    return valor     
