ed9a4d53b109bf92018942d2bab117888d399f15
Rapha29 <122626805+Rapha29@users.noreply.github.com>
2023-08-11 01:04:29 -0300

    cws

--- 22-LinguagemDeProgramacao/08-CodigosDaMateria/ArgsKwargs.py
+++ 22-LinguagemDeProgramacao/08-CodigosDaMateria/ArgsKwargs.py
@@ -1,10 +1,12 @@
+
 def exemplo(*args, **kwargs):
     print("Argumentos posicionais:")
     for arg in args:
         print(arg)
 
-    print("\nArgumentos de palavra-chave:")
+    print("\nArgumentos do tipo palavra-chave:")
     for chave, valor in kwargs.items():
         print(f'{chave}: {valor}')
 
 exemplo(1, 2, 3, nome='Rapha', idade=36)
+
--- 22-LinguagemDeProgramacao/08-CodigosDaMateria/model.py
+++ 22-LinguagemDeProgramacao/08-CodigosDaMateria/model.py
@@ -1,32 +1,22 @@
-def busca_binaria(lista, elemento):
-  minimo = 0
-  maximo = len(lista) - 1
-  encontrado = False
-  indices = []
+import pandas as pd
+# importa bibliotecas e voce pode usar um alias para abreviar a biblioteca
+# como por exemplo "pandas as pd"
+dataFrame = {
+    'Survived': [0, 1, 1, 1, 0],
+    'Pclass': [3, 1, 3, 1, 3],
+    'Name': ['Mr. Owen Harris Braund',
+             'Mrs. John Bradley (Florence Briggs Thayer Cumings)',
+             'Miss. Laina Heikkinen',
+             'Mrs Jacques Heath (Lily May Peel) Futrelle',
+             'Mr. William Henry Allen'],
+    'Sex': ['male', 'female', 'female', 'female', 'male'],
+    'Age': [22.0, 38.0, 26.0, 35.0, 35.0],
+    'Siblings/Spouses Aboard': [1, 1, 0, 1, 0],
+    'Parents/Children Aboard': [0, 0, 0, 0, 0],
+    'Fare': [7.2500, 71.2833, 7.9250, 53.1000, 8.0500]
+}
+df_titanic = pd.DataFrame(dataFrame)
+print(df_titanic)
 
-  while minimo <= maximo and not encontrado:
-    meio_lista = (minimo + maximo) // 2
-    indices.append(meio_lista)
-    if lista[meio_lista] == elemento:
-      encontrado = True
-    else:
-      if elemento < lista[meio_lista]:
-        maximo = meio_lista - 1
-      else:
-        minimo = meio_lista + 1
-
-  return encontrado, indices
-
-
-def imprime_indices(indices):
-  for i in indices:
-    print(i)
-
-
-testelista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-encontrado, indices = busca_binaria(testelista, 1)
-if encontrado:
-  print("Elemento encontrado!")
-else:
-  print("Elemento não encontrado.")
-imprime_indices(indices)
+homensnotitanic = df_titanic['male']
+print(homensnotitanic)
